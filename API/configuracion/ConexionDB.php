c5f749c5e0c8a444b9073c1532ac7657c2b47215
Leo <leo@uas.com>
2026-02-27 18:58:42 -0700

    add .
    Todavia no le entiendo

--- API/configuracion/ConexionDB.php
+++ API/configuracion/ConexionDB.php
@@ -1,39 +1,34 @@
 <?php
 
+class Conexion {
 
-class conexion {
-    
-    public static $conn = null;
+    private static $conn = null;
 
-    public static function conectar(){
-        if(self::$conn !== null){
+    public static function Conectar() {
+
+        if (self::$conn !== null) {
             return self::$conn;
         }
 
-        require_once(__DIR__ . './API/configuracion/variables.php');
+        require_once __DIR__ . '/variables.php';
 
-        if (!isset($DB_HOST) || !isset($DB_USER) ){
-            die("Error: Archivo de configuración incompleto.");
-        }
+        $dsn = "mysql:host={$DB_HOST};port={$DB_PORT};dbname={$DB_NAME};charset=utf8mb4";
 
-        $dsn = "mysql:host=" . $DB_HOST . ";dbname=" . $DB_NAME . ";charset=utf8mb4";
 
         try {
             self::$conn = new PDO($dsn, $DB_USER, $DB_PASS);
-            
+
             self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-            
+
             return self::$conn;
 
         } catch (PDOException $e) {
-            echo "Error de conexión: " . $e->getMessage();
-            die();
+            die("Error de conexión: " . $e->getMessage());
         }
-        // Método estático para cerrar
-        }
-        public static function cerrar() {
-            self::$conn = null;
-        }
+    }
 
+    public static function cerrar() {
+        self::$conn = null;
+    }
 }
