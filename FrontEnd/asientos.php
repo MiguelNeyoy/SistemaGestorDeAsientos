8be980c107f09c32721209756fe653f9ca6be970
MiguelNeyoy <miguelneyoycn@gmail.com>
2026-04-12 11:48:49 -0700

    FIX/ Se arreglo problema con la sesion en middleware y asientos, asicional un problema en el renderezidao de la tabla

--- FrontEnd/asientos.php
+++ FrontEnd/asientos.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+  session_start();
+}
 
 require_once "config.php";
 require_once "auth_middleware.php";
--- FrontEnd/auth_middleware.php
+++ FrontEnd/auth_middleware.php
@@ -1,7 +1,10 @@
 <?php
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
-function verify_access($rolesPermitidos = ['alumno', 'admin']) {
+function verify_access($rolesPermitidos = ['alumno', 'admin'])
+{
     $auth = [
         'isLoggedIn' => false,
         'tipo' => 'guest',
@@ -13,17 +16,17 @@ function verify_access($rolesPermitidos = ['alumno', 'admin']) {
         $auth['isLoggedIn'] = true;
         $auth['tipo'] = 'admin';
         $auth['token'] = $_SESSION['admin_token'];
-    } 
+    }
     // Verificar si es un Alumno
     else if (isset($_SESSION['jwt_token'])) {
         $auth['isLoggedIn'] = true;
-        $auth['tipo'] = $_SESSION['tipo'] ?? 'alumno'; 
+        $auth['tipo'] = $_SESSION['tipo'] ?? 'alumno';
         $auth['token'] = $_SESSION['jwt_token'];
     }
 
     // Verificar si el rol recuperado tiene permiso en la matriz exigida
     if ($auth['isLoggedIn'] && in_array($auth['tipo'], $rolesPermitidos)) {
-        return $auth; 
+        return $auth;
     }
 
     // Flujo bloqueado, expulsa al usuario al index
